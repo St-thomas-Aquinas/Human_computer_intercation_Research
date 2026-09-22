945c93f1072bb1743bb1f79f99ffd0c967bb6a7a
St-thomas-Aquinas <maxkuria254@gmail.com>
2026-09-23 00:03:13 +0300

    Create shows.js

---
+++ software/shows.js
@@ -0,0 +1,7 @@
+const shows = [
+    { name: "Citizen TV", url: "https://www.youtube.com/watch?v=QR68tERO10g" },
+    { name: "KTN TV", url: "https://www.youtube.com/watch?v=wd6e85PVwt0" },
+    { name: "KCB TV", url: "https://www.youtube.com/watch?v=p5B0PTJwoD4" },
+    { name: "ZEE DUNIA", url: "https://www.youtube.com/watch?v=gCLVeuTJDgg" },
+     { name: "Capuchin tv", url: "https://www.youtube.com/@CapuchinTv" }
+];
